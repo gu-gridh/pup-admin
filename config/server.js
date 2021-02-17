c1ee48f07462e4372ec6ae51a915e6747b878bb2
Arild Matsson <arild.matsson@gu.se>
2021-02-17 14:17:13 +0100

    Set server.proxy

--- config/server.js
+++ config/server.js
@@ -1,7 +1,8 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-  url: env('URL', ''),
+  url: env('URL'),
+  proxy: !!env('URL'),
   admin: {
     auth: {
       secret: env('ADMIN_JWT_SECRET'),
